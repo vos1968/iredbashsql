95aeb4ea61ee05119f718928d703611e4e816280
Pat Meredith <pat@aofh.us>
2020-02-21 18:00:03 -0600

    Working on it...

--- iredbashsql.sh
+++ iredbashsql.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+#set -x
+
+# Exit if /tmp/lock.file exists
+[ -f /tmp/iredbashsql.file ] && exit
+
+# Create lock file, sleep 1 sec and verify lock
+echo $$ > /tmp/iredbashsql.file
+sleep 1
+[ "x$(cat /tmp/iredbashsql.file)" == "x"$$ ] || exit
+
+mysql=/usr/bin/mysql
+user=vmailadmin
+pass=...
+
+
+
+# Remove lock file
+rm /tmp/iredbashsql.file
+exit 0
